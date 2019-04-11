547369cde596a1e57dc49ff5aeb19dc31cbff018
David Chambers <dlchambersjr@gmail.com>
2019-04-10 21:35:12 -0700

    adds demo

---
+++ class-03/demos/handlebars/scripts/neighborhood.js
@@ -0,0 +1,38 @@
+let neighborhoods = [];
+
+// REVIEW: This is another way to use a constructor to duplicate an array of raw data objects
+
+function Neighborhood(rawDataObject) {
+
+  // This will iterate over the object and assign the property name to the key variable
+  for (let key in rawDataObject) {
+    console.log('key', key);
+
+    // when using a variable name to identify the property, we MUST use bracket [] notation.
+    this[key] = rawDataObject[key];
+  }
+}
+
+Neighborhood.prototype.toHtml = function () {
+  // 1. Get the template from the HTML document
+  let $template = $('#neighborhood-template').html();
+  console.log('source', $template);
+  // 2. Use Handlebars to "compile" the HTML
+  let compiledTemplate = Handlebars.compile($template);
+
+  // 3. Do not forget to return the HTML from this method
+  // and... put it in the DOM.
+  return compiledTemplate(this);
+
+};
+
+// Create a new instance of Neighborhood from the data set.
+neighborhoodDataSet.forEach(neighborhoodObject => {
+  neighborhoods.push(new Neighborhood(neighborhoodObject));
+});
+
+neighborhoods.forEach(ourNewNeighborhoodObject => {
+  // What do we need to do here to render each of the neighborhoods to the DOM?
+  $('#neighborhoods').append(ourNewNeighborhoodObject.toHtml());
+
+});
